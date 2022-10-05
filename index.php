9f994472a51b0fa15a2c77a0fa1e190ec23b245d
Jamestyb <108416016+Jamestyb@users.noreply.github.com>
2022-10-05 11:52:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PHP">
+    <meta name="keywords" content="PHP">
+    <meta name="author" content="Felipe alexandre">
+    <title>PHP</title>
+</head>
+<body>
+
+        <?php
+            echo "Olá, eu sou um script PHP!";
+        ?>    
+
+</body>
+</html>
